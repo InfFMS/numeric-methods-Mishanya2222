d80c6ded1ca7e9150d89fec73a700c80d38a0e4f
Mishanya2222 <146630550+Mishanya2222@users.noreply.github.com>
2025-02-15 11:10:52 +0700

    Update Approximation.py

--- Approximation.py
+++ Approximation.py
@@ -6,6 +6,9 @@
 # cos(x) + 2x – 3 = 0
 # C точностью до 0.01.
 from matplotlib import pyplot as plt
+import math
+
+num = int(input())
 
 lst_x = []
 lst_y = []
@@ -13,20 +16,41 @@ lst_y = []
 def slove_equation(a ,b,eps):
     while b - a >= 2*eps:
         c = (a + b)/2
-        if eq(a)*eq(c) <= 0:
+        if eq(a,num)*eq(c,num) <= 0:
             a = c
         else:
             b = c
     return c
 
 
-def eq(x):
-    t = x**3 - x +1
+def eq(x,num):
+    if num ==1:
+        t = x**3 - x +1
+    elif num ==2:
+        t = x**3 -x**2 - x*9 + 9
+    elif num == 3:
+        t = x**2 - math.exp(x)
+    elif num == 4:
+        t = 5*x - 6*math.log(x) - 7
+    elif num == 5:
+        t = math.cos(x) + 2*x - 3
     return t
 
+
+#если искать корни  логаритфма, то x >0 должен быть
 for i in range(-100,100):
     lst_x.append(i)
-    lst_y.append(eq(i))
+    lst_y.append(eq(i,num))
+
+
+
+
+
+
+
+
+
+
 
 # def serch_a(a):
 #     b =0
@@ -51,6 +75,6 @@ plt.plot(lst_x,lst_y)
 plt.grid()
 plt.show()
 
-print(slove_equation(-100, 100, 0.01))
+print(slove_equation(1, 100, 0.01))
 
 
